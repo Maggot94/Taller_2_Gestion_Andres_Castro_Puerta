bd1ea59ecb4ac26bd73862051c1ee4feb4807aeb
Maggot94 <andrescastropuerta@gmail.com>
2017-08-22 23:23:14 -0500

    Todo
    Taller2_Completo

---
+++ Assets/Scripts/Bisiesto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bisiesto: MonoBehaviour {
+
+    [SerializeField]
+    private int entrada;
+
+    [SerializeField]
+    private InputField Casilla;
+
+    [SerializeField]
+    private Text Answer; 
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Casilla.text != null)
+        {
+            entrada = Int32.Parse(Casilla.text);
+        }
+       
+        if (EsBisiesto(entrada))
+        {
+            Answer.text = "Es Bisiesto";
+        }
+        else
+        {
+            Answer.text = "No Es Bisiesto";
+        }
+    }
+
+    public bool EsBisiesto( int Entrada)
+    {
+        if (Entrada % 4 == 0)
+        {
+            if (Entrada % 100 == 0)
+            {
+                if (Entrada % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Editor/Test1.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest {
+
+   
+
+    [Test]
+	public void EsMultiploDe4_1984() {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(1984));
+        
+
+    }
+    [Test]
+    public void EsMultiploDe4_2012()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(2012));
+    }
+
+    [Test]
+    public void EsMultiploDe4_2011()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(2011));
+    }
+
+    [Test]
+    public void EsMultiploDe100_1800()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(1800));
+    }
+
+    [Test]
+    public void EsMultiploDe100_1900()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(1900));
+    }
+  
+    [Test]
+    public void EsMultiploDe400_2000()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(2000));
+    }
+
+    [Test]
+    public void EsMultiploDe400_1600()
+    {
+        Bisiesto bi = new Bisiesto();
+        //bool b;
+        //b = bi.EsBisiesto(1986);
+        Assert.That(bi.EsBisiesto(1600));
+    }
+
+
+}
